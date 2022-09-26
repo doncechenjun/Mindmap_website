ad5b1287e6034e0d5fee408b3e9f0b4fb0df5297
doncechenjun <68586729+doncechenjun@users.noreply.github.com>
2022-09-26 12:42:29 +0800

    Delete src directory

--- src/Blocks.js
+++
@@ -1,9 +0,0 @@
-import Block from './Block'
-
-const Blocks=({Nodes,Event})=>{
-    return(
-            Nodes.map((Node) => {
-                return <Block Node={Node} Event={Event}/>
-    }))}
-
-export default Blocks
